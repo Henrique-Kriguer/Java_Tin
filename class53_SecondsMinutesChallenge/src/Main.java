edee20866ba245f135bbb95bb5ad4ebb44882c6d
Henrique Kriguer <henriquekriguer@gmail.com>
2024-03-19 17:22:17 -0300

    refatorando metodo conversao segundos, minutos

--- class53_SecondsMinutesChallenge/src/Main.java
+++ class53_SecondsMinutesChallenge/src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println( getDurationString(56, 5600));
-        System.out.println( getDurationString(8960));
+        System.out.println( getDurationString(65, 45));
+        System.out.println( getDurationString(3945));
     }
 
     public static String getDurationString(int seconds) {
         if( seconds <0 ){
 
-            return "Invalid data !";
+            return "Invalid data for secounds! ( " + seconds + " ) Must be positive integer value.";
         }else {
 
             int hours = seconds / 3600;
@@ -23,6 +23,12 @@ public class Main {
 
     }
    public static String getDurationString(int minutes, int seconds) {
+       if (minutes<0 ){
+           return "Invalid data for ( " + minutes + " )minutes must be integer positive value.";
+       }else if (seconds<0 || seconds > 60){
+           return "Invalid data for ( " +seconds + " ) seconds must be positive integer between 0 to 59.";
+
+       }
        return getDurationString(seconds += minutes * 60);
    }
 }
