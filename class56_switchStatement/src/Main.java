2831be5c93d9aa0bf0f94a1862edbb80e1dcd8b6
Henrique Kriguer <henriquekriguer@gmail.com>
2024-04-02 16:22:13 -0300

    class56_switch case enhanced

--- class56_switchStatement/src/Main.java
+++ class56_switchStatement/src/Main.java
@@ -10,21 +10,30 @@ public class Main {
 //            System.out.println("Value was not 1 or 2");
 //        }
 
-        int switchValue = 7;
+//        int switchValue = 7;
+//
+//        switch (switchValue){
+//            case 1:
+//                System.out.println("Value was 1 ");
+//                break;
+//            case 2:
+//                System.out.println("Value was 2");
+//                break;
+//            case 3: case 4: case 5:
+//                System.out.println(" Values was 3, 4 or 5");
+//                System.out.println("Value was " + switchValue);
+//            default:
+//                System.out.println(" Value wasn´t 1, 2, 3, 4 or 5");
+//                break;
+//        }
 
-        switch (switchValue){
-            case 1:
-                System.out.println("Value was 1 ");
-                break;
-            case 2:
-                System.out.println("Value was 2");
-                break;
-            case 3: case 4: case 5:
-                System.out.println(" Values was 3, 4 or 5");
-                System.out.println("Value was " + switchValue);
-            default:
-                System.out.println(" Value wasn´t 1, 2, 3, 4 or 5");
-                break;
-        }
+        int switchValue = 7;
+        switch(switchValue){
+            case 1 -> System.out.println(" Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> System.out.println("value was 3, 4 or 5");
+            default -> {System.out.println("value was't 1,2,3,4,or 5");
+                        System.out.println("Actually was a "+ switchValue);
+        }}
     }
 }
