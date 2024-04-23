b73261ccb50f863ba44a293a3f9f17071ba712a1
Henrique Kriguer <henriquekriguer@gmail.com>
2024-04-23 14:40:03 -0300

    Class61_TheForStatement_PrimeNumberChallenge_update

--- class61_ForStatementChallenge_isPrimeNumber/src/Main.java
+++ class61_ForStatementChallenge_isPrimeNumber/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int number = 99;
+        int number = 1369;
         System.out.println(number +" is " + (isPrime(number)? "" : "NOT") + " a prime number.");
     }
     public static boolean isPrime(int number){
@@ -8,10 +8,12 @@ public class Main {
             return number==2; // return false if number is different from 2;
         }
         for(int divisor = 2; divisor <= number/2; divisor ++){
+            //System.out.println(divisor);
             if(number % divisor == 0) {
-                return false;
+                System.out.println(number + " is divisible by "+ divisor);
+                return false; // if some divisor are found, return false means number is not prime .
             }
         }
-    return true;
+    return true; // if no divisor found, the number is prime.
     }
 }
