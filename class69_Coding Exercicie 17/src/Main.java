98a2eda1a366139ce61f7d5cf2066674babe72e8
Henrique Kriguer <henriquekriguer@gmail.com>
2024-05-08 09:29:35 -0300

    Class69_Challenge18_done

--- class69_Coding Exercicie 17/src/Main.java
+++ class69_Coding Exercicie 17/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(sumFirstAndLastDigit(432113));
+        System.out.println(sumFirstAndLastDigit(1235));
     }
     public static int sumFirstAndLastDigit (int number){
         int lastDigit = number %10;
