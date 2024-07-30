cb40acb901d547b6d0900da2de38b3e752294cfc
Henrique Kriguer <henriquekriguer@gmail.com>
2024-07-30 19:37:29 -0300

    Class78_setter

--- class76_Intro_OOP/src/Car.java
+++ class76_Intro_OOP/src/Car.java
@@ -1,14 +1,13 @@
 public class Car {
 
-    private String make = "Porsche";
-    private String model = "Carrera";
+    private String make = "Tesla";
+    private String model = "X";
     private String color ;
     private int doors ;
     private boolean convertible ;
 
-    public String getMake() {
-        return make;
-    }
+    public String getMake() {  return make; }
+
 
     public String getModel() {
         return model;
@@ -25,8 +24,33 @@ public class Car {
     public boolean isConvertible() {
         return convertible;
     }
+    public void setMake(String make){
+        if(make==null) make="Unknown";
+        String lowerCaseMake = make.toLowerCase();
+        switch (lowerCaseMake){
+            case "holden", "porsche", "tesla" -> this.make = make;
+            default -> {this.make = "Unsupportade";
+            }
+        }
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
 
     public void describeCar(){
-        System.out.println(doors + "-Door " + color + " " + make + " " + model + " " +  convertible);
+        System.out.println(doors + "-Door " + color + " " + make + " " + model + " " +  "Is convertible? " + convertible);
     }
 }
--- class76_Intro_OOP/src/Main.java
+++ class76_Intro_OOP/src/Main.java
@@ -3,8 +3,23 @@ public class Main {
     public static void main(String[] args) {
 
         Car car = new Car();
+        car.setMake("Porsche");
+        car.setModel("GT");
+        car.setColor("black");
+        car.setDoors(2);
+        car.setConvertible(true);
         car.describeCar();
         System.out.println("make: "+car.getMake());
         System.out.println("model: "+car.getModel());
+
+        Car car2 = new Car();
+        car2.setMake("tesla");
+        car2.setModel("X");
+        car2.setColor("grey");
+        car2.setDoors(4);
+        car2.setConvertible(false);
+        car2.describeCar();
+        System.out.println("make: "+car2.getMake());
+        System.out.println("model: "+car2.getModel());
     }
 }
