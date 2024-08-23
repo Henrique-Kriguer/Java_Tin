34d7b98a4b9995c813b2668b065db71bf454820f
Henrique Kriguer <henriquekriguer@gmail.com>
2024-08-23 14:10:12 -0300

    Class87_codingExercise34_Carpet

--- class87_codingexercise_33_Point/src/Point.java
+++ class87_codingexercise_33_Point/src/Point.java
@@ -3,16 +3,18 @@ public class Point {
     private int x;
     private int y;
     public Point(){
-
+        this.x=0;
+        this.y=0;
     }
     public Point(int x, int y){
-
+        this.x=x;
+        this.y=y;
     }
     public int getX(){
-        return x;
+        return this.x;
     }
     public int getY(){
-        return y;
+        return this.y;
     }
     public void setX(int x){
         this.x = x;
