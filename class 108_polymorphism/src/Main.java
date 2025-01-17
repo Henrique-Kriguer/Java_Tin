747deae8103332197c5e291c6bd44ec433b1d442
Henrique-Kriguer <henrique.kriguer@gmail.com>
2025-01-17 15:43:47 -0300

    Class 109 polymorphism 2

--- class 108_polymorphism/src/Main.java
+++ class 108_polymorphism/src/Main.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Movie movie = new Comedy("Star wars");
-        movie.watchMovie();
+        //Movie movie = new Comedy("Star wars");
+        //movie.watchMovie();
 
+//        Movie theMovie = Movie.getMovie("Adventure", "Star wars");
+//        theMovie.watchMovie();
+//
+//        Movie theMovie2 = Movie.getMovie("Science Fiction", "Alliens");
+//        theMovie2.watchMovie();
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter the movie type (A for adventure, C for comedy, S for Science Fiction or Q to quit: ");
+            String type = sc.nextLine();
+            if (type.equalsIgnoreCase("Q")) { //A função equalsIgnoreCase é um método da classe String em Java. Ela é usada para comparar duas strings, ignorando as diferenças entre maiúsculas e minúsculas.
+                break;
+            }
+            System.out.println("Enter the movie title: ");
+            String title = sc.nextLine();
+            Movie theMovie = Movie.getMovie(type, title);
+            theMovie.watchMovie();
+        }
     }
 }
--- class 108_polymorphism/src/Movie.java
+++ class 108_polymorphism/src/Movie.java
@@ -6,13 +6,22 @@ public class Movie {
         this.title = title;
     }
 
-    public void watchMovie(){
+    public void watchMovie() {
 
         String instanceType = this.getClass().getSimpleName();
         System.out.println("Watching " + instanceType + " - " + title);
     }
-}
 
+    public static Movie getMovie(String type, String title) {
+    // This kind of method returns a new instance object, is called a factory method.
+        return switch (type.toUpperCase().charAt(0)) {
+            case 'A' -> new Adventure(title);
+            case 'C' -> new Comedy(title);
+            case 'S' -> new SciFi(title);
+            default -> new Movie(title);
+        };
+    }
+}
 class Adventure extends Movie {
     public Adventure(String title) {
         super(title);
@@ -41,4 +50,19 @@ class Adventure extends Movie {
                     "Comedy Music",
                     "Happy Ending");
         }
-}
+    }
+    class SciFi extends Movie {
+    public SciFi(String title) {
+                super(title);
+            }
+
+            @Override
+            public void watchMovie() {
+                super.watchMovie();
+                System.out.printf(" .. %s%n".repeat(3),
+                        "The bad Alliens do bad things",
+                        "Scarly Music",
+                        "Some star will explode");
+            }
+        }
+
