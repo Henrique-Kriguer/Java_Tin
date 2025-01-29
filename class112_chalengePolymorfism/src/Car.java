ea0bb6254506a22acccb7a678ef53d0ca1863002
Henrique-Kriguer <henrique.kriguer@gmail.com>
2025-01-29 16:59:35 -0300

    Class 113_chalengeExerciciePolymorphism

--- class112_chalengePolymorfism/src/Car.java
+++ class112_chalengePolymorfism/src/Car.java
@@ -46,3 +46,58 @@ class GasPoweredCar extends Car {
         System.out.printf("GasPoweredCar -> usage exceeds the average: %.2f %n", avgKmPerLiter);
     }
 }
+
+class HybridCar extends Car {
+    private double avgKmPerCharge;
+    private int batterySize;
+    private int cylinders;
+
+
+    public HybridCar(String description) {
+        super(description);
+    }
+
+    public HybridCar(String description, double avgKmPerCharge, int batterySize, int cylinders) {
+        super(description);
+        this.avgKmPerCharge = avgKmPerCharge;
+        this.batterySize = batterySize;
+        this.cylinders = cylinders;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.printf("Hybrid -> %d cylinders are fired up!%n", cylinders);
+        System.out.printf("Hybrid -> %d kwh battery is on!%n", batterySize);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("Hybrid -> usage exceeds the average: %.2f %n", avgKmPerCharge);
+    }
+}
+
+class ElectricCar extends Car {
+    private double avgKmPerCharge;
+    private int batterySize;
+
+
+    public ElectricCar(String description) {
+        super(description);
+    }
+
+    public ElectricCar(String description, double avgKmPerCharge, int batterySize) {
+        super(description);
+        this.avgKmPerCharge = avgKmPerCharge;
+        this.batterySize = batterySize;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.printf("BEV -> switch %d kwh battery on, Ready!%n", batterySize);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("BEV -> usage exceeds the average: %.2f %n", avgKmPerCharge);
+    }
+}
--- class112_chalengePolymorfism/src/Main.java
+++ class112_chalengePolymorfism/src/Main.java
@@ -4,12 +4,18 @@ public class Main {
        Car car = new Car("2022 red barchetta");
        runRace(car);
 
-       GasPoweredCar car2 = new GasPoweredCar("2022 red barchetta", 10.5, 8);
-         runRace(car2);
+       Car ferrary = new GasPoweredCar("2022 red barchetta", 10.5, 8);
+         runRace(ferrary);
+
+       Car tesla = new ElectricCar("2022 red tesla model 3", 568, 75);
+         runRace(tesla);
+
+       Car ferraryHybrid = new HybridCar("2022 black Ferrary sf90 Stradale", 16.5, 8, 8);
+         runRace(ferraryHybrid);
     }
 
     public static void runRace(Car car){
-        car.startEngine();
-        car.drive();
+     car.startEngine();
+     car.drive();
     }
 }
